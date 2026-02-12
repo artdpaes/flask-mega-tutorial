ece5298d881e25d54a81559af7a13d543e3e40d7
Arthur <art.dpaes@gmail.com>
2026-02-12 14:51:04 +0000

    Chapter 2

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, world."
+    user = make_user("Fulano")
+
+    posts = [
+        make_post(make_user("John"), "Beautiful day in Portland!"),
+        make_post(make_user("Susan"), "The Avengers movie was so cool!")
+    ]
+
+    return render_template("index.html", title="Home", user=user, posts=posts)
+
+def make_user(username : str):
+    return {"username": username}
+
+def make_post(user, body):
+    return {"author": user, "body": body}
